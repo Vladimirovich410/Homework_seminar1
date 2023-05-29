338294261e1e656a155e640e304bf6646c9593a1
Vladimir <vermakov410@gmail.com>
2023-05-29 23:55:40 +0300

    error?

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает 
+// два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Привет!");
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if(num1 > num2) max = num1;
+{
+    Console.WriteLine(num1+">"+num2);
+    Console.WriteLine("max = "+max);
+}
+else if(num2 > num1) max = num2;
+{
+    Console.WriteLine(num2+">"+num1);
+    Console.WriteLine("max = "+max);
+}
+else if(num1 == num2);
+{
+    Console.WriteLine("Эти числа равны");
+}
